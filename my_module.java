fbda1b5f86a36ce8c6b1b020d52ed44edc9b2613
HwangHarim <scopan01@naver.com>
2021-09-17 20:38:25 +0900

    Add my_module [sum]

--- my_module.java
+++ my_module.java
@@ -4,8 +4,20 @@ public static void main(String args){
     result = a+b;
 
     System.out.println("a+b="+result);
+    
     prints();
+    calculator(int a, int b);
+    
     public void prints(){
         System.out.print(Hello);
     }
+
+    public int calculator(int a, int b){
+        this.a = a;
+        this.b = b;
+        int sum = a+b;
+        System.out.println(sum);
+        return sum;
+    }
+
 }

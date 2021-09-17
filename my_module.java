80f4b985403c9a2db82d81ee6b4be8fffea6d031
HwangHarim <scopan01@naver.com>
2021-09-17 17:50:24 +0900

    Add my_moudle [Sub]

--- my_module.java
+++ my_module.java
@@ -2,5 +2,10 @@ public static void main(String args){
     int a = 5;
     int b = 3;
     result = a+b;
+
     System.out.println("a+b="+result);
+    prints();
+    public void prints(){
+        System.out.print(Hello);
+    }
 }
